565cfe5507b9733cf92013aa8012e25465c8fb53
praneet <praneet@gmail.com>
2023-04-09 14:07:40 +0530

    final

--- shop/admin.py
+++ shop/admin.py
@@ -1,8 +1,15 @@
+from atexit import register
 from django.contrib import admin
 from .models import *
-
-class CatagoryAdmin(admin.ModelAdmin):
-    list_display=('name','image','description')
-
+ 
+ 
+"""
+class CategoryAdmin(admin.ModelAdmin):
+  list_display = ('name', 'image', 'description')
+admin.site.register(Catagory,CategoryAdmin)
+"""
+ 
 admin.site.register(Catagory)
 admin.site.register(Product)
+admin.site.register(Cart)
+admin.site.register(Favourite)
